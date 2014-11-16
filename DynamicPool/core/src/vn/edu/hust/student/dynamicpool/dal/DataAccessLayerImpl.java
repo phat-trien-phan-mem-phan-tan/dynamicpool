ac5342fad64ad30b90fc414d8f68b473e5e398ad
lethanhbk55 <lethanhbk55@gmail.com>
2014-11-16 18:23:10 +0700

    joinHost

--- DynamicPool/core/src/vn/edu/hust/student/dynamicpool/dal/DataAccessLayerImpl.java
+++ DynamicPool/core/src/vn/edu/hust/student/dynamicpool/dal/DataAccessLayerImpl.java
@@ -1,5 +1,7 @@
 package vn.edu.hust.student.dynamicpool.dal;
 
+import vn.edu.hust.student.dynamicpool.controller.MainController;
+import vn.edu.hust.student.dynamicpool.exception.NetworkException;
 import vn.edu.hust.student.dynamicpool.presentation.PresentationBooleanCallback;
 
 public class DataAccessLayerImpl implements DataAccessLayer{
@@ -7,7 +9,12 @@ public class DataAccessLayerImpl implements DataAccessLayer{
 	@Override
 	public void joinHost(int key, PresentationBooleanCallback callback) {
 		// TODO Auto-generated method stub
-		
+		try {
+			MainController.getInstance().start(key);
+			callback.callback(true, null);
+		} catch (NetworkException e) {
+			callback.callback(false, e);
+		}
 	}
 
 	@Override
