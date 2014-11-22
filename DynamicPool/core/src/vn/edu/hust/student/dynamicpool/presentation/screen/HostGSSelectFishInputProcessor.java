5477a06b53382674f731ba645600b0f22832c440
Tran Quang Tien <quangtien92@gmail.com>
2014-11-22 20:02:15 +0700

    them game screen cho host

---
+++ DynamicPool/core/src/vn/edu/hust/student/dynamicpool/presentation/screen/HostGSSelectFishInputProcessor.java
@@ -0,0 +1,11 @@
+package vn.edu.hust.student.dynamicpool.presentation.screen;
+
+import vn.edu.hust.student.dynamicpool.presentation.WorldController;
+
+public class HostGSSelectFishInputProcessor extends GSSelectFishInputProcessor {
+
+	public HostGSSelectFishInputProcessor(WorldController worldController) {
+		super(worldController);
+	}
+
+}
