12ef490ebf7efe0707f805f5695973da8a02755c
Tran Quang Tien <quangtien92@gmail.com>
2014-11-27 16:56:39 +0700

    xóa code không cần thiết

--- DynamicPool/core/src/vn/edu/hust/student/dynamicpool/bll/trajectoryController/ITrajectoryController.java
+++
@@ -1,8 +0,0 @@
-package vn.edu.hust.student.dynamicpool.bll.trajectoryController;
-
-import vn.edu.hust.student.dynamicpool.model.FishState;
-
-public interface ITrajectoryController {
-
-	FishState getFishState();
-}
