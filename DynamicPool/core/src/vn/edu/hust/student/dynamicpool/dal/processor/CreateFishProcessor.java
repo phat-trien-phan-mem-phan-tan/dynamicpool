2eb39d6c4b81f0bedc993508780daaad3f04b40f
Trần Quang Tiến <quangtien92@gmail.com>
2014-11-30 11:43:11 +0700

    them class chuyen doi fish

--- DynamicPool/core/src/vn/edu/hust/student/dynamicpool/dal/processor/CreateFishProcessor.java
+++ DynamicPool/core/src/vn/edu/hust/student/dynamicpool/dal/processor/CreateFishProcessor.java
@@ -4,29 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import vn.edu.hust.student.dynamicpool.bll.model.CycleTrajectory;
-import vn.edu.hust.student.dynamicpool.bll.model.ETrajectoryType;
-import vn.edu.hust.student.dynamicpool.bll.model.Fish;
 import vn.edu.hust.student.dynamicpool.bll.model.IFish;
-import vn.edu.hust.student.dynamicpool.bll.model.LineTrajectory;
-import vn.edu.hust.student.dynamicpool.bll.model.NoneTrajectory;
-import vn.edu.hust.student.dynamicpool.bll.model.SinTrajectory;
 import vn.edu.hust.student.dynamicpool.dal.statics.Field;
 import vn.edu.hust.student.dynamicpool.events.EventDestination;
 import vn.edu.hust.student.dynamicpool.events.EventType;
-import flexjson.JSONDeserializer;
-import flexjson.JSONSerializer;
 
 public class CreateFishProcessor extends Processor {
-	JSONDeserializer<Fish> deserializer = new JSONDeserializer<Fish>();
-	JSONSerializer serializer = new JSONSerializer();
 
 	@Override
 	public ProcessorExecutionResponse execute(ProcessorExecutionRequest request) {
 		Map<String, Object> map = request.getParameters();
 		if (map.containsKey(Field.FISH) && map.containsKey(Field.CLIENT_NAME)) {
-			ETrajectoryType trajectoryType = getTrajectoryType(map);
-			IFish fish = getFish(map, trajectoryType);
+			IFish fish = FishParser.deserializerFish(map);
 			String clientName = (String) map.get(Field.CLIENT_NAME);
 			List<Object> list = new ArrayList<Object>();
 			list.add(fish);
@@ -36,38 +25,6 @@ public class CreateFishProcessor extends Processor {
 		}
 		return null;
 	}
-
-	private IFish getFish(Map<String, Object> map,
-			ETrajectoryType trajectoryType) {
-		String fishString = serializer.exclude("*.class").serialize(
-				map.get(Field.FISH));
-		switch (trajectoryType) {
-		case LINE:
-			return deserializer.use("trajectory", LineTrajectory.class)
-					.deserialize(fishString, Fish.class);
-		case CYCLE:
-			return deserializer.use("trajectory", CycleTrajectory.class)
-					.deserialize(fishString, Fish.class);
-		case SIN:
-			return deserializer.use("trajectory", SinTrajectory.class)
-					.deserialize(fishString, Fish.class);
-		default:
-			return deserializer.use("trajectory", NoneTrajectory.class)
-					.deserialize(fishString, Fish.class);
-		}
-	}
-
-	private ETrajectoryType getTrajectoryType(Map<String, Object> map) {
-		if (map != null && map.containsKey("trajectory")) {
-			Map<String, Object> trajectoryMap = ((Map<String, Object>) map
-					.get("trajectory"));
-			if (trajectoryMap != null
-					&& trajectoryMap.containsKey("trajectoryType")) {
-				String trajectoryTypeMap = (String) map.get("trajectoryType");
-				if (map != null)
-					return ETrajectoryType.valueOf(trajectoryTypeMap);
-			}
-		}
-		return ETrajectoryType.NONE;
-	}
+	
+	
 }
--- DynamicPool/core/src/vn/edu/hust/student/dynamicpool/dal/processor/SendFishProcessor.java
+++ DynamicPool/core/src/vn/edu/hust/student/dynamicpool/dal/processor/SendFishProcessor.java
@@ -2,14 +2,10 @@ package vn.edu.hust.student.dynamicpool.dal.processor;
 
 import java.util.Map;
 
-import com.eposi.eventdriven.models.AbstractModel.fields;
-
-import vn.edu.hust.student.dynamicpool.bll.model.Fish;
+import vn.edu.hust.student.dynamicpool.bll.model.IFish;
 import vn.edu.hust.student.dynamicpool.dal.statics.Field;
 import vn.edu.hust.student.dynamicpool.events.EventDestination;
 import vn.edu.hust.student.dynamicpool.events.EventType;
-import flexjson.JSONDeserializer;
-import flexjson.JSONSerializer;
 
 public class SendFishProcessor extends Processor {
 
@@ -19,11 +15,7 @@ public class SendFishProcessor extends Processor {
 		Map<String, Object> params = request.getParameters();
 		if (params.containsKey(Field.FISH)
 				&& params.containsKey(Field.SUCCESSFUL)) {
-			JSONDeserializer<Fish> deserializer = new JSONDeserializer<Fish>();
-			JSONSerializer serializer = new JSONSerializer();
-			String fishString = serializer.exclude("*.class").serialize(
-					params.get(Field.FISH));
-			Fish fish = deserializer.deserialize(fishString);
+			IFish fish = FishParser.deserializerFish(params);
 			boolean isSuccess = (boolean) params.get(Field.SUCCESSFUL);
 			if (isSuccess) {
 				EventDestination.getInstance().dispatchSuccessEventWithObject(
